ced49af3cd203ed1b90e7e02c75132e2dd91b6b1
Owen Craston <owen.craston@shopify.com>
2018-11-29 22:24:14 -0500

    clean up code

--- app/src/main/java/com/example/owencraston/androidmovies/MainActivity.kt
+++ app/src/main/java/com/example/owencraston/androidmovies/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.owencraston.androidmovies
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -30,8 +31,10 @@ class MainActivity : AppCompatActivity() {
     fun fetchMovies() {
         println("fetching movie data")
         val videoCollection : Result
+        val list_id = "96662"
+        val api_key = "204bb7c38f35e758cd8918f5a6c38aa7"
 
-        disposable = movieApiServe.getMovies("96662", 1, "204bb7c38f35e758cd8918f5a6c38aa7")
+        disposable = movieApiServe.getMovies(list_id, 1, api_key)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
@@ -40,8 +43,8 @@ class MainActivity : AppCompatActivity() {
                     )
     }
 
-    fun onError(throwable: Throwable) {
-        val name="hello"
+    fun onError(error: Throwable) {
+        Log.i("Android Movies", "Error getting data", error)
     }
 
     // dispose of if request fails
