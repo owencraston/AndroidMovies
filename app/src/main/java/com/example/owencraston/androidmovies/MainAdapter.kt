81c4e0da87f0ace8638e836437dd61d059d49fd1
Owen Craston <owen.craston@shopify.com>
2018-11-29 23:30:04 -0500

    load images with picasso

--- app/src/main/java/com/example/owencraston/androidmovies/MainAdapter.kt
+++ app/src/main/java/com/example/owencraston/androidmovies/MainAdapter.kt
@@ -4,7 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.owencraston.androidmovies.R.id.imageView_movie_poster
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.movie_row.view.*
+
 
 class MainAdapter(): RecyclerView.Adapter<CustomViewHolder>()  {
 
@@ -20,7 +23,9 @@ class MainAdapter(): RecyclerView.Adapter<CustomViewHolder>()  {
     }
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val movie = movieCollection.get(position)
+        val movie_backdrop_url = "https://image.tmdb.org/t/p/original/${movie.backdrop_path}"
         holder.view.textView_movie_title.text = movie.title
+        Picasso.get().load(movie_backdrop_url).into(holder?.view?.imageView_movie_poster)
     }
 
     fun setData(data : List<Movie>) {
